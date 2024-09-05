18f33e16060fbb55aff696bf2ad91cc25edae62d
Tsiry Marcel <14010248+mntm@users.noreply.github.com>
2024-09-05 20:15:41 +0300

    models: Item class
    holds all information about a paint

---
+++ apps/catalog/lib/models/item.dart
@@ -0,0 +1,19 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class Item {
+  final int id;
+  final String name;
+  final Color color;
+  final int price = Random().nextInt(100);
+
+  Item(this.id, this.name)
+      : color = Colors.primaries[id % Colors.primaries.length];
+
+  @override
+  int get hashCode => id;
+
+  @override
+  bool operator ==(Object other) => other is Item && other.id == id;
+}
