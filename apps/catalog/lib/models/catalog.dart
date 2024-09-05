9363870f045059e4142b88725bf7a65751239fa8
Tsiry Marcel <14010248+mntm@users.noreply.github.com>
2024-09-05 20:16:09 +0300

    models: Hard coded catalog of paints

---
+++ apps/catalog/lib/models/catalog.dart
@@ -0,0 +1,33 @@
+import 'package:paint_catalog/models/item.dart';
+
+class Catalog {
+  static List<String> itemNames = [
+    'Code Smell',
+    'Control Flow',
+    'Interpreter',
+    'Recursion',
+    'Sprint',
+    'Heisenbug',
+    'Spaghetti',
+    'Hydra Code',
+    'Off-By-One',
+    'Scope',
+    'Callback',
+    'Closure',
+    'Automata',
+    'Bit Shift',
+    'Currying',
+  ];
+
+  /// Get item by [id].
+  ///
+  /// In this sample, the catalog is infinite, looping over [itemNames].
+  Item getById(int id) => Item(id, itemNames[id % itemNames.length]);
+
+  /// Get item by its position in the catalog.
+  Item getByPosition(int position) {
+    // In this simplified case, an item's position in the catalog
+    // is also its id.
+    return getById(position);
+  }
+}
