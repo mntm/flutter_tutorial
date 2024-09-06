a1646e2dfa0898acc15b66915f3d42f8e5355ba1
Tsiry Marcel <14010248+mntm@users.noreply.github.com>
2024-09-06 15:03:34 +0300

    chore: initialized Provider

--- apps/catalog/lib/main.dart
+++ apps/catalog/lib/main.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:paint_catalog/controllers/cart_controller.dart';
+import 'package:paint_catalog/controllers/catalog_controller.dart';
 import 'package:paint_catalog/models/catalog.dart';
 import 'package:paint_catalog/screens/cart_screen.dart';
 import 'package:paint_catalog/screens/catalog_screen.dart';
+import 'package:paint_catalog/services/cart_service.dart';
+import 'package:paint_catalog/services/catalog_service.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MainApp());
+  final Catalog catalog = Catalog();
+  runApp(
+    MultiProvider(
+      providers: [
+        Provider(
+          create: (_) => CatalogController(
+            CatalogService(catalog),
+          ),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => CartController(
+            CartService(catalog),
+          ),
+        ),
+      ],
+      child: const MainApp(),
+    ),
+  );
 }
 
 final _router = GoRouter(
