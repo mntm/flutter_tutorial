128f4169040082e8d65ac4d16e826c233d5199b5
Tsiry Marcel <14010248+mntm@users.noreply.github.com>
2024-09-11 09:05:49 +0300

    chore: removed unused files

--- apps/catalog_bloc/lib/src/catalog/catalog_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:paint_collection/paint_collection.dart';
-
-class CatalogController {
-  final CatalogService _service;
-
-  CatalogController(this._service);
-
-  CatalogService get catalogService => _service;
-}
