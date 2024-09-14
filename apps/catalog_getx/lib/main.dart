61fcfe76ef9e34437f360a2f3f6634af370eeffe
Tsiry Marcel <14010248+mntm@users.noreply.github.com>
2024-09-14 09:55:22 +0300

    Registered an instance of Repository

--- apps/catalog_getx/lib/main.dart
+++ apps/catalog_getx/lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:catalog_getx/src/data/repository.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import 'src/app.dart';
 
 void main() {
+  Get.lazyPut<Repository>(
+    () => Repository(),
+  );
   runApp(
     const MainApp(),
   );
