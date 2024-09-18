5c2bb16c6450958b30849ab5cea37711c00708eb
Tsiry Marcel <14010248+mntm@users.noreply.github.com>
2024-09-18 15:38:47 +0300

    fix: throttle events to prevent being rate limited by the API

--- apps/intermediate/api_fetch_bloc/lib/src/posts/bloc/post_bloc.dart
+++ apps/intermediate/api_fetch_bloc/lib/src/posts/bloc/post_bloc.dart
@@ -2,16 +2,27 @@ import 'dart:async';
 
 import 'package:api_fetch_bloc/src/posts/data/post_repository.dart';
 import 'package:api_fetch_bloc/src/posts/models/post_item.dart';
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stream_transform/stream_transform.dart';
 
 part "post_event.dart";
 part "post_state.dart";
 
+/// throttle events by dropping events emitted during [duration].
+/// No event is emitted during [duration]
+EventTransformer<E> throttleEvents<E>(Duration duration) {
+  return (events, mapper) {
+    return droppable<E>().call(events.throttle(duration), mapper);
+  };
+}
+
 class PostBloc extends Bloc<PostEvent, PostState> {
   PostBloc(this.repo)
       : super(const PostState(status: PostStatus.initial, hasNext: true)) {
-    on<PostRequested>(_onPostRequested);
+    on<PostRequested>(_onPostRequested,
+        transformer: throttleEvents(const Duration(milliseconds: 300)));
   }
 
   final PostRepository repo;
